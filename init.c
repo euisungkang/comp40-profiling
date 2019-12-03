27eedce5fabdd31c067761c937e44cd6c83c4fff
Easton Kang <euisungkang@gmail.com>
2019-12-02 21:28:14 -0500

    removed um struct

--- init.c
+++ init.c
@@ -22,17 +22,8 @@ Last Modified: 11/19/2019
 #include <sys/stat.h>
 #include "instructions.h"
 
-typedef struct UM {
-        Segment segmem;
-        uint32_t *registers;
-        uint32_t counter;
-} *UM;
-
 /* Function Prototypes */
-UM UM_new();
-void UM_free(UM *);
-void run_instructions(UM, short, short, short, short, uint32_t);
-uint32_t *read_program(UM, char *);
+void run_instructions(Segment, uint32_t *, uint32_t, short, short, short, short, uint32_t);
 
 int main(int argc, char *argv[])
 {
@@ -42,7 +33,9 @@ int main(int argc, char *argv[])
                 exit(EXIT_FAILURE);
         }
 
-        UM um = UM_new();
+        Segment segmem = Seg_New();
+        uint32_t *registers = calloc(8, sizeof(uint32_t));
+        uint32_t counter = 1;
 
         FILE *input = fopen(argv[1], "r");
         struct stat sb;
@@ -50,105 +43,99 @@ int main(int argc, char *argv[])
         int size = sb.st_size/4 + 1;
         uint32_t *m0 = malloc(sizeof(uint32_t)*size);
         char byteArray[4] = {0};
-        int counter = 1;
+        int index = 1;
         int i = 3;
 
         m0[0] = size;
         while(fscanf(input, "%c", &(byteArray[i])) == 1) {
                 if(i == 0) {
-                        m0[counter++] = *(uint32_t *) byteArray;
+                        m0[index++] = *(uint32_t *) byteArray;
                         i = 4;
                 }
                 i--;
         }
         fclose(input);
         
-        Seq_addhi(um->segmem->m, m0);
-        um->segmem->seg_count++;
+        Seq_addhi(segmem->m, m0);
+        segmem->seg_count++;
 
 
-        uint32_t *zero = Seq_get(um->segmem->m, 0);
-        while(um->counter < zero[0]) {
+        uint32_t *zero = Seq_get(segmem->m, 0);
+        while(counter < zero[0]) {
                 short ra = 0, rb = 0, rc = 0;
                 uint32_t value = 0;
                 
-                uint32_t code = (uint32_t) Bitpack_getu(zero[um -> counter],
+                uint32_t code = (uint32_t) Bitpack_getu(zero[counter],
                                                         4, 28);
                 if(code < 13) {
-                        ra = (uint32_t) Bitpack_getu(zero[um -> counter], 3, 6);
-                        rb = (uint32_t) Bitpack_getu(zero[um -> counter], 3, 3);
-                        rc = (uint32_t) Bitpack_getu(zero[um -> counter], 3, 0);
+                        ra = (uint32_t) Bitpack_getu(zero[counter], 3, 6);
+                        rb = (uint32_t) Bitpack_getu(zero[counter], 3, 3);
+                        rc = (uint32_t) Bitpack_getu(zero[counter], 3, 0);
                 }else {
-                        ra = (uint32_t) Bitpack_getu(zero[um -> counter], 3, 25);
-                        value = (uint32_t) Bitpack_getu(zero[um -> counter],
+                        ra = (uint32_t) Bitpack_getu(zero[counter], 3, 25);
+                        value = (uint32_t) Bitpack_getu(zero[counter],
                                                         25, 0);
                 }
 
-                run_instructions(um, code, ra, rb, rc, value);
+                run_instructions(segmem, registers, counter, code, ra, rb, rc, value);
                 if(code != 12) {
-                        (um->counter)++;
+                        (counter)++;
                 } else {
-                        zero = Seq_get(um->segmem->m, 0);
+                        zero = Seq_get(segmem->m, 0);
                 }
         }
-        exit(EXIT_SUCCESS);
-}
 
-UM UM_new()
-{
-        UM um = malloc(sizeof(struct UM));
-        um -> segmem = Seg_New();
-        um -> registers = calloc(8, sizeof(uint32_t));
-        um -> counter = 1;
-        return um;
+
+        free(registers);
+        exit(EXIT_SUCCESS);
 }
 
 /* Input: UM, opcode, register index A, B, C, value
    Determines which instruction is run through the opcode. A, B, C
    are passed in as actual register addresses. Entire condition structure
    is switch cases */
-void run_instructions(UM um, short opcode, short A, short B,
+void run_instructions(Segment segmem, uint32_t *reg, uint32_t counter, short opcode, short A, short B,
                         short C, uint32_t value)
 {
         switch(opcode) {
                 case 0:
-                        if (um -> registers[C] != 0) {
-                                um -> registers[A] = um -> registers[B];
+                        if (reg[C] != 0) {
+                                reg[A] = reg[B];
                         }
                         break;
                 case 1:
-                        load(um -> segmem, &um -> registers[A],
-                            um -> registers[B], um -> registers[C]);
+                        load(segmem, &reg[A],
+                            reg[B], reg[C]);
                         break;
                 case 2:
-                        store(um -> segmem, &um -> registers[A],
-                                &um -> registers[B], &um -> registers[C]);
+                        store(segmem, &reg[A],
+                                &reg[B], &reg[C]);
                         break;
                 case 3:
-                        um -> registers[A] = (um -> registers[B] + um -> registers[C]);
+                        reg[A] = (reg[B] + reg[C]);
                         break;
                 case 4:
-                        um -> registers[A] = (um -> registers[B] * um -> registers[C]);
+                        reg[A] = (reg[B] * reg[C]);
                         break;
                 case 5:
-                        um -> registers[A] = (uint32_t) floor(um -> registers[B] / um -> registers[C]);
+                        reg[A] = (uint32_t) floor(reg[B] / reg[C]);
                         break;
                 case 6:
-                        um -> registers[A] = ~(um -> registers[B] & um -> registers[C]);
+                        reg[A] = ~(reg[B] & reg[C]);
                         break;
                 case 7:
-                        UM_free(&um);
+                        free(reg); 
                         exit(EXIT_SUCCESS);
                         break;
                 case 8:
-                        map(um -> segmem, &um -> registers[B],
-                                &um -> registers[C]);
+                        map(segmem, &reg[B],
+                                &reg[C]);
                         break;
                 case 9:
-                        unmap(um -> segmem, &um -> registers[C]);
+                        unmap(segmem, &reg[C]);
                         break;
                 case 10:
-                        printf("%c", (char)um -> registers[C]);
+                        printf("%c", (char)reg[C]);
                         break;
                 case 11:
                         ;
@@ -157,30 +144,20 @@ void run_instructions(UM um, short opcode, short A, short B,
                                 in = (char) getc(stdin);
                         }
                         if (in == EOF) {
-                                um -> registers[C] = 1;
+                                reg[C] = 1;
                                 break;
                         }
-                        um -> registers[C] = in;
+                        reg[C] = in;
                         break;
                 case 12:
-                        load_program(um -> segmem, &um -> counter, &um ->
-                                     registers[B], &um -> registers[C]);
+                        load_program(segmem, &counter, &reg[B], &reg[C]);
                         break;
                 case 13:
-                        um -> registers[A] = value;
+                        reg[A] = value;
                         break;
                 default:
                         fprintf(stderr, "Invalid Instruction\n");
                         exit(EXIT_FAILURE);
                         break;
         }
-}
-
-/* Input: UM *
-   Free's the UM machine. Free's the segmem as well using Seg_Free */
-void UM_free(UM *um)
-{
-    Seg_Free(&((*um) -> segmem));
-    free((*um) -> registers);
-    free(*um);
 }
